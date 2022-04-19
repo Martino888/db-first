55b2b97f99088b304bcf5080b3f3675c8e560824
Martino888 <maxmartino33@gmail.com>
2022-04-19 16:03:31 +0200

    tipologia dei dati

--- index.php
+++ index.php
@@ -2,14 +2,39 @@
 $db=[
     [
         'id'=> 0,
-
+        'immagine'=> "",
+        'marca'=> '',
+        'modello'=> '',
+        'motore'=> '',
+        'anno'=> '',
+        'posti'=> '',
+        'marce'=> '',
+        'cilindra'=> '',
+        'alimentazione'=> '',
     ],
     [
         'id'=> 1,
+        'immagine'=> "",
+        'marca'=> '',
+        'modello'=> '',
+        'motore'=> '',
+        'anno'=> '',
+        'posti'=> '',
+        'marce'=> '',
+        'cilindra'=> '',
+        'alimentazione'=> '',
 
     ],    [
         'id'=> 2,
-
+        'immagine'=> "",
+        'marca'=> '',
+        'modello'=> '',
+        'motore'=> '',
+        'anno'=> '',
+        'posti'=> '',
+        'marce'=> '',
+        'cilindra'=> '',
+        'alimentazione'=> '',
     ],
 ];
 ?>
